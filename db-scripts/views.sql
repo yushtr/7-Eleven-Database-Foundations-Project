8b4c8e7ed7a9e68f1c53bc290a223b557f154afe
Ayush Tripathi <yushhhhhtr@gmail.com>
2026-07-05 12:53:29 +0700

    Add incoming_deliveries_view

--- db-scripts/views.sql
+++ db-scripts/views.sql
@@ -3,3 +3,11 @@ SELECT p.product_id, p.product_name, c.category_id, c.category_name, c.subcatego
 FROM products AS p
 JOIN categories as c USING (category_id)
 JOIN store_inventory as s USING (product_id);
+
+CREATE VIEW incoming_deliveries_view AS 
+SELECT d.delivery_date, d.driver_name, d.driver_phone, d.license_plate, di.product_id, p.product_name, c.category_name, c.subcategory_name, di.quantity_delivered, di.expiration_date
+FROM delivery_items AS di 
+JOIN products AS p USING (product_id)
+JOIN categories AS c USING (category_id)
+JOIN deliveries AS d USING (delivery_id);
+
