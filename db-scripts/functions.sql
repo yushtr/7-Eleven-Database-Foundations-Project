24b4bdd095db41b76d49be718d4794b014dbc4f8
Ayush Tripathi <yushhhhhtr@gmail.com>
2026-07-05 10:43:34 +0700

    Fix fn_check_expired_products function and removed last function because of redundancy

--- db-scripts/functions.sql
+++ db-scripts/functions.sql
@@ -4,7 +4,7 @@ $$
 SELECT COALESCE(SUM(total_amount), 0.00) AS revenue
 FROM sales_transactions
 WHERE store_id = $1 
-    AND transaction_date BETWEEN $2 AND $3
+    AND transaction_date BETWEEN $2 AND $3;
 $$ LANGUAGE SQL;
 
 CREATE OR REPLACE FUNCTION fn_get_branch_expenses(target_store_id INT, start_date DATE, end_date DATE)
@@ -13,26 +13,20 @@ $$
 SELECT COALESCE(SUM(amount), 0.00) AS expense
 FROM store_expenses
 WHERE store_id = $1 
-    AND expense_date BETWEEN $2 AND $3
+    AND expense_date BETWEEN $2 AND $3;
 $$ LANGUAGE SQL; 
 
 CREATE OR REPLACE FUNCTION fn_get_branch_profits(target_store_id INT, start_date DATE, end_date DATE) 
 RETURNS DECIMAL(15,2) AS 
 $$ 
-SELECT fn_get_branch_revenue($1, $2, $3) - fn_get_branch_expenses($1, $2, $3)
+SELECT fn_get_branch_revenue($1, $2, $3) - fn_get_branch_expenses($1, $2, $3);
 $$ LANGUAGE SQL; 
 
-CREATE OR REPLACE FUNCTION fn_check_expired_products(target_store_id INT, current_date DATE) 
+CREATE OR REPLACE FUNCTION fn_check_expired_products(target_store_id INT) 
 RETURNS SETOF store_inventory AS 
 $$
-SELECT product_id, stock_quantity
+SELECT *
 FROM store_inventory
 WHERE store_id = $1
-    AND DATE = $2
+    AND expiration_date <= CURRENT_DATE;
 $$ LANGUAGE SQL; 
-
-CREATE OR REPLACE FUNCTION fn_check_expired_products_relation_display(target_store_id INT, current_date DATE)
-RETURNS SETOF store_inventory AS 
-$$
-SELECT (fn_check_expired_products($1, $2)).*
-$$ LANGUAGE SQL;
